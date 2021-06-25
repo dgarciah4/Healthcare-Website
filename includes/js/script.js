1cda8e3c28d5bb9b548fc034adaa9f997e53ff67
ComplexityAverse <learb@uab.edu>
2021-06-25 12:26:46 -0500

    added .gitignore local

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,21 +1,73 @@
 
 var sectionList = ["mainPage","resume","contact","portfolio"];
 
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {});
+
+function navigateClaims(insuranceId){
+
+window.location.href="claims.html?insuranceId=" + insuranceId;
+
+
+}
+
+function claim1DetailsbuttonClick(){
+
+myModal.show();
+
+
+}
+
+
+
+
 
 function insurance1buttonClick(){
+  //BSL TODO maybe here we have a service that gets the contents of the div and replaces the existing row instead of revealing a div
+  //THEN, the save will return it to normal using a different service
+  //That would be consistent with the loading of the data as well
 
   document.getElementById("insurance1div").classList.remove("hiddenSection");
   document.getElementById("insurance1Editdiv").classList.remove("hiddenSection");
 
   document.getElementById("insurance2div").classList.add("hiddenSection");
   document.getElementById("insurance2div").classList.remove("row");
+  document.getElementById("insurance3div").classList.add("hiddenSection");
+  document.getElementById("insurance3div").classList.remove("row");
+  document.getElementById("insuranceAddButtondiv").classList.add("hiddenSection");
+  document.getElementById("insuranceAddButtondiv").classList.remove("row");
+
+
 
   //document.getElementById("insurance2Editdiv").classList.add("hiddenSection");
 
 }
 
+function insuranceAddClearButtonClick(){
+  document.getElementById("addType").value="";
+  document.getElementById("addProvider").value="";
+  document.getElementById("addGroup").value="";
+
+}
+
+function insuranceAddSaveButtonClick(){
+
+
+reloadPageClick();
+  //document.getElementById("insurance1div").classList.remove("hiddenSection");
+
+}
+
+function insuranceAddbuttonClick(){
+
+
+  document.getElementById("insuranceAddSavediv").classList.remove("hiddenSection");
+  document.getElementById("insuranceAddButtondiv").classList.add("hiddenSection");
+
+
+}
+
 function reloadPageClick(){
-  alert("use web service to reload after save");
+  alert("use web service to save then use another to refresh contents or have web service return an object");
 
 location.reload();
   //window.reloadPage();
